c56ed8b2ceb7901d35beb5a00ea0212ffec3125d
AwesomeSam9523 <samakshgupta04@gmail.com>
2026-02-20 02:36:36 +0530

    Add User interface to Express namespace for enhanced type definitions

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,5 +1,13 @@
+import type { User } from "@prisma/client";
+
 declare global {
   namespace Express {
+    interface User {
+      id: User["id"];
+      email: User["email"];
+      name: User["name"];
+      avatarUrl: User["avatarUrl"];
+    }
   }
 }
 
